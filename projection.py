73eb5787cba21881a3f50003e29d5999cb6ab443
Cao Zhong Z <zhong.z.cao@intel.com>
2025-01-25 11:57:07 +0800

    Ajudt bandwidth efficiency arbitrarily
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- projection.py
+++ projection.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 # hardware
-B = 864*1000*1000*1000
-T = 733*1000*1000*1000*1000
+B_eff = 0.87
+B = 864*1000*1000*1000 * B_eff
+
+T_eff = 1
+T = 733*1000*1000*1000*1000 * T_eff
+
 kv_dtype = 1
 w_dtype = 1
 fw_dtype = 2
