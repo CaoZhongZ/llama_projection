ea0b1260fa02823527393cdc404d76c7c7881f36
Cao Zhong Z <zhong.z.cao@intel.com>
2025-01-26 09:27:14 +0800

    Import from config file
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- projection.py
+++ projection.py
@@ -1,15 +1,7 @@
 import numpy as np
 
 # hardware
-B_eff = 0.87
-B = 864*1000*1000*1000 * B_eff
-
-T_eff = 1
-T = 733*1000*1000*1000*1000 * T_eff
-
-kv_dtype = 1
-w_dtype = 1
-fw_dtype = 2
+from hw_config import *
 
 # model
 Hidden = 4096
