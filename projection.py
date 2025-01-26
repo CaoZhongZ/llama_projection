ac6d00021f124fee3378966d32c9d28c64203784
Cao Zhong Z <zhong.z.cao@intel.com>
2025-01-26 08:06:40 +0800

    Add more counts
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- projection.py
+++ projection.py
@@ -41,6 +41,7 @@ logitw_size = Hidden * Vocab
 
 prompt_kv = 2 * N * Prompt * KVHidden * kv_dtype
 time_rope = prompt_kv / B
+time_rsmnorm = 3 * prompt_kv / B
 
 sdpa = 2 * N * Prompt * Prompt * Headdim * AttHeads
 if not Causal:
@@ -49,7 +50,7 @@ if not Causal:
 time_sdpa = sdpa / T
 
 # prefill roughly equal to promp compute time in each block
-time_prefill_1block = 2 * Prompt * N * C / T + time_rope + time_sdpa
+time_prefill_1block = 2 * Prompt * N * C / T + time_rope + time_sdpa + time_rsmnorm
 time_prefill_model = time_prefill_1block * Block
 
 # Generation in 1 transformer block, kv-history and model
@@ -65,11 +66,13 @@ time_gen_1block = size_kv / B + max(weight_sz/B, gen_ops/T)
 time_gen_model = time_gen_1block * Block
 
 # Last gemm for the logits, non-trivial if large vocabulary
-time_next = max(logitw_size*fw_dtype/B, 2*N*logitw_size/(T/fw_dtype))
+time_next = max(logitw_size*fw_dtype/B, 2*N*logitw_size/(T/(fw_dtype/w_dtype)))
 
 # total time prefill, logit gemm and generation of each token
 time = time_prefill_model + time_next + (time_gen_model + time_next) * Gen
 
+# Output with confidence info. Gen/Prefill ratio gives intuition how each portion affect final t/s
+# Compute/Memory ratio show whether should downgrade memory efficiency
 print("KV-cache size", t_size_kv/1024/1024, "MB")
 print("Time = ", time)
 print("T/s = ", N * (Gen + Prompt)/time)
@@ -77,4 +80,4 @@ print("Prefill time", time_prefill_model + time_next)
 print("Next time", time_gen_model + time_next)
 print("Generation time", (time_gen_model + time_next) * Gen)
 print("Gen:Prefill ratio", (time_gen_model + time_next) * Gen / (time_prefill_model + time_next))
-
+print("Compute/Memory ratio", (gen_ops/T)/(weight_sz/B))
